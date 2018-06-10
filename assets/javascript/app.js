68829a0c8773999686fe3d025031eaf2aef761ec
lextiel <cummins.a88@gmail.com>
2018-06-10 13:40:03 -0400

    initial html, css, and js content

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+//create a variable for the Q+A
+//this variable is an array containing multiple objects (key value pairs)
+//each question/answer is an object that can be called with JS
+var questionsAnswers = [
+    { 
+        question: "What is the name of Hermione Granger's pet cat?";
+        answer: ["Hedwig", "Crookshanks", "Scabbers", "Kreacher"];
+        correctAnswer:  "Crookshanks";
+
+    }
+    { 
+        question: "What is the name of the wizarding prison?";
+        answer: ["Alcatraz", "Azkaban", "Alderaan", "Azazel"];
+        correctAnswer: "Azkaban";
+
+    }
+    { 
+        question: "Of the objects named, which one is not one of Voldemort's horcruxes?";
+        answer: ["The Diadem of Ravenclaw", "The Sword of Gryffindor", "Regulus Black's Locket", "Marvolo Gaunt's Ring"];
+        correctAnswer: "The Sword of Gryffindor";
+    }
+
+
+]
+
+
+
+//create click event for start button that hides the button
+//create a function to start the timer
+//and shows the question/answer divs
+//populate question div with first question
+//populate answers div with first set of answers as individual buttons
+//create click event that captures the user input
+//compare user input with correct answer
+
