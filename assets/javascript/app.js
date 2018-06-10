b15893be6a903e690b5d8dbaf944a9bf41e6a52c
lextiel <cummins.a88@gmail.com>
2018-06-10 14:12:30 -0400

    onclick

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,35 @@
 //each question/answer is an object that can be called with JS
 var questionsAnswers = [
     { 
-        question: "What is the name of Hermione Granger's pet cat?";
-        answer: ["Hedwig", "Crookshanks", "Scabbers", "Kreacher"];
-        correctAnswer:  "Crookshanks";
+        question: "What is the name of Hermione Granger's pet cat?",
+        answer: ["Hedwig", "Crookshanks", "Scabbers", "Kreacher"],
+        correctAnswer:  "Crookshanks",
 
     }
     { 
-        question: "What is the name of the wizarding prison?";
-        answer: ["Alcatraz", "Azkaban", "Alderaan", "Azazel"];
-        correctAnswer: "Azkaban";
+        question: "What is the name of the wizarding prison?",
+        answer: ["Alcatraz", "Azkaban", "Alderaan", "Azazel"],
+        correctAnswer: "Azkaban",
 
     }
     { 
-        question: "Of the objects named, which one is not one of Voldemort's horcruxes?";
-        answer: ["The Diadem of Ravenclaw", "The Sword of Gryffindor", "Regulus Black's Locket", "Marvolo Gaunt's Ring"];
-        correctAnswer: "The Sword of Gryffindor";
+        question: "Of the objects named, which one is not one of Voldemort's horcruxes?",
+        answer: ["The Diadem of Ravenclaw", "The Sword of Gryffindor", "Regulus Black's Locket", "Marvolo Gaunt's Ring"],
+        correctAnswer: "The Sword of Gryffindor",
     }
 
 
 ]
 
+//create click event that grabs the start button & hides the button 
+//var hideButton = document.getElementById("startButton");
+document.onkeyup = function() {
+$("#startButton").hide();
+$("div").show();
+}
 
 
-//create click event for start button that hides the button
+//and populates the question div with the first random question
 //create a function to start the timer
 //and shows the question/answer divs
 //populate question div with first question
