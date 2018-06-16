84d8f52ba1a988e6a571841e43cd8f95963cd69f
lextiel <cummins.a88@gmail.com>
2018-06-16 19:18:04 -0400

    functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,13 @@ var questionsAnswers = [
         answer: ["Hedwig", "Crookshanks", "Scabbers", "Kreacher"],
         correctAnswer:  "Crookshanks",
 
-    }
+    },
     { 
         question: "What is the name of the wizarding prison?",
         answer: ["Alcatraz", "Azkaban", "Alderaan", "Azazel"],
         correctAnswer: "Azkaban",
 
-    }
+    },
     { 
         question: "Of the objects named, which one is not one of Voldemort's horcruxes?",
         answer: ["The Diadem of Ravenclaw", "The Sword of Gryffindor", "Regulus Black's Locket", "Marvolo Gaunt's Ring"],
@@ -23,19 +23,60 @@ var questionsAnswers = [
 
 ]
 
-//create click event that grabs the start button & hides the button 
-//var hideButton = document.getElementById("startButton");
-document.onkeyup = function() {
-$("#startButton").hide();
-$("div").show();
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+var questionsAsked = 0;
+var questionsToAnswer= 5;
+var timeToAnswer =15;
+var result = 1;
+var ticker = 1;
+
+
+
+//create a function that randomizes the questions/answers + populates Q+A
+function randomQuestion () {
+   questionAnswers[Math.floor(Math.random() * questionAnswers.length)];
+
 }
 
 
-//and populates the question div with the first random question
-//create a function to start the timer
-//and shows the question/answer divs
-//populate question div with first question
-//populate answers div with first set of answers as individual buttons
-//create click event that captures the user input
+//create a decrement function that starts the timer
+//and times out when finished
+function countdown(){
+	if(ticker != 1) {
+		timeToAnswer--;
+		$("#timer").text(timeToAnswer);
+		if(timeToAnswer === 0) {
+			timeToAnswer = 15;
+			ticker = 1;
+			alert("Time's Up!");
+		}
+	}
+}
+
+
+//create click event that grabs the start button & hides the button 
+//as well as reveals the timer, random question, & answer choices
+$("#startButton").on("click", function(){
+    $("#startButton").hide();
+    $("div").show();
+    $("#timer").text("Time to Answer: " + timeToAnswer);
+    countdown();
+});
+
+
+
+//create a function that populates question div with the first random question
+
+
+//create a function to populate answers div with first set of answers as individual buttons
 //compare user input with correct answer
+
+
+//uLTIMATELY: on the click event
+//shuffle the questions--> replace the opening screen w/ questions and answers
+//set the interval countdown to 5,000; which populates the question
+//populate the question =
+
 
